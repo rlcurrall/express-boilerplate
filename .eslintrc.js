1102b21fabea44b92c41d8e66852663874305a9c
Robb Currall <rlcurra@castlebranch.com>
2019-09-11 12:04:19 -0400

    Clean up dir and add new scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  env: {
+    node: true
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
